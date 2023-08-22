680b7bfeb2702d76ab89be2f875d5dc93c54fd6a
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-08-22 16:02:10 +0100

    test build-image job

--- push_docker.sh
+++ push_docker.sh
@@ -2,5 +2,5 @@ dockerpath=techyshell/capstone
 echo "Docker ID and Image: $dockerpath"
 docker tag capstone techyshell/capstone:v1.0.0
 docker images
-docker login
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push techyshell/capstone:v1.0.0
