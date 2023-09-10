ee94ba7016fe6a0c0ba5ab54d6d9949fa4e3488f
Rachelle Maconeghy <rnmaconeghy@outlook.com>
2023-09-10 14:25:28 +0100

    kubectl fix

--- eksctl.sh
+++ eksctl.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 eksctl version
